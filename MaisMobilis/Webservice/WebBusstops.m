1f31c6cab16fd412370a7cc25898fc04781af303
Telmo Marques <coldhx@gmail.com>
2011-12-28 18:39:55 +0000

    Added Webservice folder & 3 web service classes

---
+++ MaisMobilis/Webservice/WebBusstops.m
@@ -0,0 +1,13 @@
+//
+//  WebBusstops.m
+//  MaisMobilis
+//
+//  Created by tom on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "WebBusstops.h"
+
+@implementation WebBusstops
+
+@end
---
+++ MaisMobilis/Webservice/WebLines.h
@@ -0,0 +1,13 @@
+//
+//  WebLines.h
+//  MaisMobilis
+//
+//  Created by tom on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebLines : NSObject
+
+@end
