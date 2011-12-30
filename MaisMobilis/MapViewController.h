8c471c9fdc62fa7bd01b6916588358e14ccbcb34
Rita Silva <rita.lauz@gmail.com>
2011-12-30 22:53:05 +0000

    storyboard initialized… map view controller added

---
+++ MaisMobilis/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  MaisMobilis
+//
+//  Created by Rita Silva on 12/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
