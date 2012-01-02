8d878b2322bd0e59f691399ce892752c149aa688
Rita Silva <rita.lauz@gmail.com>
2012-01-02 04:26:30 +0000

    Revert 1a6137279a3adace7f9bcdb294a93fb5738a6c5e^..HEAD

---
+++ MaisMobilis/ReferencePoint.m
@@ -0,0 +1,18 @@
+//
+//  ReferencePoint.m
+//  MaisMobilis
+//
+//  Created by tom on 1/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ReferencePoint.h"
+
+
+@implementation ReferencePoint
+
+@dynamic latitude;
+@dynamic longitude;
+@dynamic referencePointID;
+
+@end
